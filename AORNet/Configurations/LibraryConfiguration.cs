0bcdc8203bfe84635f76f50d58ca5c502c10dbb4
Raagh <pattferraggi@gmail.com>
2017-03-03 10:31:03 -0300

    changed LibraryConfiguration

--- AORNet/Configurations/LibraryConfiguration.cs
+++ AORNet/Configurations/LibraryConfiguration.cs
@@ -8,7 +8,7 @@ namespace AORNet.Configurations
 {
     public static class LibraryConfiguration
     {
-        public static string ServerName = "FuriusAo";
+        public static string ServerName = "FuriusAO";
         public static int StructAddress = 0x007C04C4;
         public static int OpenForReading = 0x001F0FFF;
     }
